9aeaf92359aea023a8d562aad17eba6662f8efe9
joseph bartels <joebartels@hotmail.com>
2015-11-17 12:00:11 -0500

    work on dummy application/documentation

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -15,8 +15,8 @@ module.exports = function(environment) {
     },
     contentSecurityPolicy: {
       'default-src': "'none'",
-      'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
-      'script-src': "'self' https://gist.github.com",
+      'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com https://assets-cdn.github.com",
+      'script-src': "'self' https://cdnjs.cloudflare.com https://gist.github.com",
       'font-src': "'self' https://fonts.gstatic.com data:", // Allow fonts to be loaded from http://fonts.gstatic.com
       // 'img-src': "'self'",
       // 'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
